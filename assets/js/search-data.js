1cf77ee821524a7a3bffd75f9d66d2ee69bf409b
jeremyfix <Jeremy.fix@gmail.com>
2025-03-11 10:42:04 +0000

    Deploying to gh-pages from @ jeremyfix/jeremyfix.github.io@7ee2327b4dd2adf55d0b8e535cfa61204df9ab04 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -59,17 +59,6 @@ ninja.data = [{
           window.location.href = "/blog/2025/oneliner/";
         
       },
-    },{id: "post-a-post-with-code",
-      
-        title: "a post with code",
-      
-      description: "an example of a blog post with some code",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2015/code/";
-        
-      },
     },{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
